cbd89614435cc2af16999451b2d5411eada54095
Yue Du <ifduyue@gmail.com>
2019-11-24 21:32:57 +0800

    __all__

--- xxhash/__init__.py
+++ xxhash/__init__.py
@@ -13,4 +13,6 @@ from ._xxhash import (xxh32, xxh32_digest, xxh32_intdigest,
 VERSION = '1.4.3'
 __all__ = ['xxh32', 'xxh32_digest', 'xxh32_intdigest', 'xxh32_hexdigest',
            'xxh64', 'xxh64_digest', 'xxh64_intdigest', 'xxh64_hexdigest',
+           'xxh3_64_digest', 'xxh3_64_intdigest', 'xxh3_64_hexdigest',
+           'xxh3_128_digest', 'xxh3_128_intdigest', 'xxh3_128_hexdigest',
            'VERSION', 'XXHASH_VERSION']
